893a46d8a0f0bd052b264fa4dd1c6435f7ecbfad
swerg <swerg@swerg.ru>
2020-12-16 22:31:14 +0700

    Supporting Lua5.4 is added
    +improve code

--- make-bin-arc.cmd
+++ make-bin-arc.cmd
@@ -1,10 +1,12 @@
 md .bin\x32
 md .bin\x64-Lua51
 md .bin\x64-Lua53
+md .bin\x64-Lua54
 
 copy build\VC2015\.build\Win32\Release\*.dll .bin\x32
 copy build\VC2015\.build\x64\Release\*.dll .bin\x64-Lua51
-copy build\VC2015\.build\x64\Release-Lua53\*.dll .bin\x64-Lua53
+copy build\VC2015\.build\x64\Release53\*.dll .bin\x64-Lua53
+copy build\VC2015\.build\x64\Release54\*.dll .bin\x64-Lua54
 
 copy nul .bin\ReadMe.txt
 echo Библиотека расширения w32 для Lua. >> .bin\ReadMe.txt
@@ -13,7 +15,8 @@ echo Подробное описание: https://quik2dde.ru/viewtopic.php?id=78 >> .bin\ReadMe.
 echo. >> .bin\ReadMe.txt
 echo \x32       -- для QUIK 6.x, 7.x >> .bin\ReadMe.txt
 echo \x64-Lua51 -- для QUIK 8.0-8.4 >> .bin\ReadMe.txt
-echo \x64-Lua53 -- для QUIK 8.5 и далее >> .bin\ReadMe.txt
+echo \x64-Lua53 -- для QUIK 8.5 и далее при выборе Lua5.3 для выполнении скрипта >> .bin\ReadMe.txt
+echo \x64-Lua54 -- для QUIK 8.11 и далее при выборе Lua5.4 для выполнении скрипта >> .bin\ReadMe.txt
 
 del /Q w32.dll.zip 
 
